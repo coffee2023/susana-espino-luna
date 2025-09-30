fac8a4e1b76804bfa1f916639b5e3c6172447d7a
coffee2023 <susig1212@gmail.com>
2025-09-30 19:38:48 -0400

    form and functionality added

--- index.js
+++ index.js
@@ -23,3 +23,38 @@ const copyright = document.createElement('p');
 copyright.textContent =`Susana Espino-Garcia ${thisYear} \u00A9`;
 footer.appendChild(copyright);
 //===============================================================
+
+//add event listener to the submit button on form. console log values
+const messageForm = document.querySelector('form[name="leave_message"]');// 'form[name="leave_message"]' looks for form with inputed name
+messageForm.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const userName = event.target.userName.value;
+    const userEmail = event.target.userEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+    console.log(userName, userEmail, usersMessage);
+
+    //add messages in list
+    const messageSection = document.getElementById("messages");
+    const messageList = document.querySelector("#messages ul");//only gets the ul element
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `
+        <a href="mailto:${userEmail}"> ${userName}'s message: </a>
+        <span>${usersMessage}</span>
+    `;
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = `remove`;
+    removeButton.type = "button";
+    removeButton.addEventListener("click", () => {
+        const entry = removeButton.parentNode;
+        entry.remove();//remove <li> from dom
+    });
+    
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    event.target.reset();//will reset form after submit
+
+   
+
+});
